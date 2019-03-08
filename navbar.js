e9b9798e7065917a38cbfb7ff9480a6e5f663397
matteoxx <mateuszniziolek96@gmail.com>
2019-03-08 16:14:45 +0100

    navbar modified

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<nav class="nav navbar"> <a class="nav-link" href="index.html">Home</a>' +
+            '<a class="nav-link" href="#">Blog</a>' +
+            '<a class="nav-link" href="#">Quiz</a>' +
+            '<a class="nav-link" href="contact.html">Kontakt</a>';
+
+let elem = document.querySelector(".navbar");
+elem.innerHTML = nav;
