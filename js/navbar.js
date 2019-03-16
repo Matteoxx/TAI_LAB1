dacced402e0a46ad81ece4d3801fdaf201815a2e
matteoxx <mateuszniziolek96@gmail.com>
2019-03-16 13:44:30 +0100

    quiz done

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 let nav = '<nav class="nav navbar"> <a class="nav-link" href="index.html">Home</a>' +
             '<a class="nav-link" href="#">Blog</a>' +
-            '<a class="nav-link" href="#">Quiz</a>' +
+            '<a class="nav-link" href="quiz.html">Quiz</a>' +
             '<a class="nav-link" href="contact.html">Kontakt</a>';
 
 let elem = document.querySelector(".navbar");
